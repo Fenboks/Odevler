9575790501961b77a34093aca33d4c8309fcfcd7
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-10-19 23:53:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Lütfen x değerini giriniz:");
+int x=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen y değerini giriniz:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen z değerini giriniz:");
+int z = Convert.ToInt32(Console.ReadLine());
+
+int hacim = x * y * z;
+
+Console.WriteLine("Dikdörtgenler prizmasının hacmi:"+hacim);

4e51effa57ca9338e218cddad76ec15480a9869a
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-10-19 23:43:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Lütfen birinci sayıyı giriniz:");
+int sayı1=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen ikinci sayıyı giriniz:");
+int sayı2 = Convert.ToInt32(Console.ReadLine());
+
+int bölüm = sayı1 / sayı2;
+
+Console.WriteLine("Girdiğiniz sayıların bölümü:"+bölüm);
+
+

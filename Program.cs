41a850e338bff2390c0596786383249ec5b8a571
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-10-19 23:38:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Lütfen birinci sayıyı giriniz:");
+int sayı1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen ikinci sayıyı giriniz:");
+int sayı2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen üçüncü sayıyı giriniz:");
+int sayı3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen dördüncü sayıyı giriniz:");
+int sayı4 = Convert.ToInt32(Console.ReadLine());
+
+int toplam = sayı1 + sayı2 + sayı3 + sayı4;
+int çarpım = sayı1 * sayı2 * sayı3 * sayı4;
+
+Console.WriteLine("Sayıların toplamı:" + toplam+" ve çarpımı:" + çarpım);

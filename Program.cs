a0ea9dc629d0067b5b82733130e6547325eb60a5
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-10-19 15:25:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int fark;
+
+Console.Write("Birinci sayıyı girin: ");
+int sayı1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("İkinci sayıyı girin: ");
+int sayı2 = Convert.ToInt32(Console.ReadLine());
+
+fark = sayı1 - sayı2;
+
+Console.WriteLine("Sonuç:"+fark);
+

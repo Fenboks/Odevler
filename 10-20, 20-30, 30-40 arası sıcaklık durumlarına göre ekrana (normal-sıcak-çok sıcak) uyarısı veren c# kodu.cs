b7408487f1fc2b51c830bde3c89890e08e272f4e
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-10-28 19:55:51 +0300

    Update and rename Program.cs to 10-20, 20-30, 30-40 arası sıcaklık durumlarına göre ekrana (normal-sıcak-çok sıcak) uyarısı veren c# kodu.cs

--- 10-20, 20-30, 30-40 arası sıcaklık durumlarına göre ekrana (normal-sıcak-çok sıcak) uyarısı veren c# kodu.cs
+++ 10-20, 20-30, 30-40 arası sıcaklık durumlarına göre ekrana (normal-sıcak-çok sıcak) uyarısı veren c# kodu.cs
@@ -1,6 +1,5 @@
 ﻿while (true)
 {
-
     Console.WriteLine("Lütfen 10 ile 40 arası sıcaklığı giriniz:");
     int sıcaklık = Convert.ToInt32(Console.ReadLine());
 
@@ -9,13 +8,11 @@
         Console.WriteLine("Normal.");
         break;
     }
-
     else if (sıcaklık == 20)
     {
         Console.WriteLine("Ne Normal Ne Yüksek.");
         break;
     }
-
     else if (20 <= sıcaklık && sıcaklık <= 29)
     {
         Console.WriteLine("Yüksek.");
@@ -26,17 +23,11 @@
         Console.WriteLine("Ne Yüksek Ne Çok Yüksek.");
         break;
     }
-
     else if (30 <= sıcaklık && sıcaklık <= 40)
     {
         Console.WriteLine("Çok Yüksek.");
         break;
     }
-
     else
             Console.WriteLine("Geçersiz sıcaklık girdiniz.");
-    
-
-   
-
 }

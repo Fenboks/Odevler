95639fc527922825ebe24fe58d64d135b4681bef
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-10-28 19:53:38 +0300

    Update and rename Program.cs to Kullanıcı adı: admin ve şifre: Admin_32453@ olduğunda giriş başarılı, değilse başarısız uyarısı veren c# kodu.cs

--- Kullanıcı adı: admin ve şifre: Admin_32453@ olduğunda giriş başarılı, değilse başarısız uyarısı veren c# kodu.cs
+++ Kullanıcı adı: admin ve şifre: Admin_32453@ olduğunda giriş başarılı, değilse başarısız uyarısı veren c# kodu.cs
@@ -3,7 +3,6 @@ string doğru_şifre="Admin_32453@";
 
 while (true)
 {
-
     Console.WriteLine("Lütfen kullanıcı adını giriniz:");
     string kullanıcı_adı = Console.ReadLine();
 
@@ -13,7 +12,6 @@ while (true)
     if (kullanıcı_adı == doğru_kullanıcı_adı && şifre == doğru_şifre)
     {
         Console.WriteLine("Giriş başarılı.");
-        
     }
     else
         Console.WriteLine("Hatalı kullanıcı adı veya şifre.");

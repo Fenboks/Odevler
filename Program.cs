6465b6ad5dae4828a2ce4513176b0125115578aa
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-10-28 19:52:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string doğru_kullanıcı_adı = "admin";
+string doğru_şifre="Admin_32453@";
+
+while (true)
+{
+
+    Console.WriteLine("Lütfen kullanıcı adını giriniz:");
+    string kullanıcı_adı = Console.ReadLine();
+
+    Console.WriteLine("Lütfen şifreyi giriniz");
+    string şifre = Console.ReadLine();
+
+    if (kullanıcı_adı == doğru_kullanıcı_adı && şifre == doğru_şifre)
+    {
+        Console.WriteLine("Giriş başarılı.");
+        
+    }
+    else
+        Console.WriteLine("Hatalı kullanıcı adı veya şifre.");
+
+    break;
+}

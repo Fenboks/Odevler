6fee0676630750d76b6eaabbf2f046541f23ca5c
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-10-28 19:46:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Lütfen bir sayı giriniz");
+int sayı=Convert.ToInt32(Console.ReadLine());
+
+if (sayı % 2 == 0)
+{
+    Console.WriteLine("Girdiğiniz sayı çiftir.");
+}
+
+else
+{
+    Console.WriteLine("Girdiğiniz sayı tektir.");
+}

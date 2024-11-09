42ba5fd33348dc14ba4d8aa8620a475d1e93286d
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-11-09 13:46:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Bir cümle giriniz: ");
+string cümle = Console.ReadLine();
+
+string[] kelimeler = cümle.Split(' ');
+
+int kelimesayısı = kelimeler.Length;
+
+Console.WriteLine("Cümledeki kelime sayısı: " + kelimesayısı);

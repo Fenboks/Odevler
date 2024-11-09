658e9ac9f900ce0d8d3af597f9e92e11dce7c2ee
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-11-09 13:43:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+ArrayList sayilar = new ArrayList { 9, 80, 59, 15, 37 };
+
+int toplam = 0; 
+
+foreach (int sayi in sayilar)
+{
+    Console.WriteLine(sayi);
+    toplam += sayi;
+}
+
+Console.WriteLine("Sayıların Toplamı: " + toplam);

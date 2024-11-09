92d5986c5590810e98ef4e028a5256e2083f9198
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-11-09 13:48:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿    Random random = new Random();
+
+    int doğrusayı = random.Next(1, 101);
+
+    Console.WriteLine("Bir sayı tahmin edin");
+while (true)
+{
+    int tahmin = Convert.ToInt32(Console.ReadLine());
+
+    if (doğrusayı == tahmin)
+    {
+        Console.WriteLine("Doğru tahmin ettin");
+        break;
+    }
+    else if (doğrusayı < tahmin)
+    {
+        Console.WriteLine("Soğuk");
+    }
+    else
+    {
+        Console.WriteLine("Sıcak");
+    }
+}
+

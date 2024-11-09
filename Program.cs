3e92efc664dfe2f67a639849b1666ed2c5d73b76
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-11-09 13:54:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿string input;
+do //while(true) da kullanılabilir buraya. Bir de ondalık sayılarda hata veriyor double ı nasıl kullanacağımı çözemedim.
+{
+    Console.WriteLine("İşlem seçin (+, -, *, /) veya 'çıkış' yazarak programdan çıkın:");
+    input = Console.ReadLine();
+
+    if (input == "çıkış")
+        break;
+
+    Console.Write("İlk sayıyı giriniz: ");
+    double sayı1=Convert.ToInt32(Console.ReadLine());
+    
+    Console.Write("İkinci sayıyı giriniz: ");
+    double sayı2 =Convert.ToInt32(Console.ReadLine());
+
+    switch (input)
+    {
+        case "+":
+            Console.WriteLine("Sonuç:"+(sayı1+sayı2));
+            break;
+        case "-":
+            Console.WriteLine("Sonuç:"+ (sayı1 - sayı2));
+            break;
+        case "*":
+            Console.WriteLine("Sonuç:"+(sayı1 * sayı2));
+            break;
+        case "/":
+            if (sayı2 != 0)
+                Console.WriteLine("Sonuç:"+(sayı1 / sayı2));
+            else
+                Console.WriteLine("Hata: Bir sayıyı sıfıra bölünemezsin.");
+            break;
+        default:
+            Console.WriteLine("Geçersiz işlem türü girdin.");
+            break;
+    }
+
+} 
+while (input != "çıkış");
+
+Console.WriteLine("Program kapanıyor..");

5bbf8f63a2c2d1d73e2497dca66a66202692e9b2
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-11-09 13:50:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+ArrayList isimler = new ArrayList();
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Bir isim girin: ");
+    string isim = Console.ReadLine();
+    isimler.Add(isim);
+}
+
+isimler.Sort();
+
+Console.WriteLine("\nAlfabetik Sıralama:");
+foreach (var isim in isimler)
+{
+    Console.WriteLine(isim);
+}

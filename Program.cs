b72a41c24fd0ec194fe7a3fab2d28a689488b312
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-11-09 13:49:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Bir sayı giriniz:");
+
+int n = int.Parse(Console.ReadLine());
+
+int a = 0, b = 1;
+
+Console.Write("Fibonacci serisi: ");
+
+for (int i = 0; i < n; i++)
+{
+    if (i == 0)
+        Console.Write(a + " ");
+    else if (i == 1)
+        Console.Write(b + " ");
+    else
+    {
+        int next = a + b;
+        Console.Write(next + " ");
+        a = b;
+        b = next; 
+    }
+}

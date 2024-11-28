39054b219f737dd3574d709b2729b3a0fd076546
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-11-28 20:57:21 +0300

    Add files via upload

---
+++ Dizideki En Büyük Değeri Bulan Parametreli ve Geriye Değer Döndüren Metot.cs
@@ -0,0 +1,20 @@
+﻿int[] sayılar = { 10, 25, 3, 42, 15 };
+
+int enbüyük = enbüyükdeğer(sayılar);
+
+    int enbüyükdeğer(int[] dizi)
+{
+    int enbüyük = dizi[0];
+
+    foreach (int sayı in dizi)
+    {
+        if (sayı > enbüyük)
+        {
+            enbüyük = sayı;
+        }
+    }
+
+    return enbüyük;
+}
+
+Console.WriteLine($"Dizideki en büyük değer: {enbüyük}");

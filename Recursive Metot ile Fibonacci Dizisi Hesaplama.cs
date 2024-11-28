d90e41ba296ea36a2b8f6843356b0cd6f9c70079
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-11-28 20:58:50 +0300

    Add files via upload

---
+++ Recursive Metot ile Fibonacci Dizisi Hesaplama.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Fibonacci dizisindeki kaçıncı sayıyı görmek istiyorsun?");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sonuç = Fibonacci(n);
+
+int Fibonacci(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+Console.WriteLine("Fibonacci sayısı:" + sonuç);

fbc2cf82553fd8eb4e33e092f66e41713f2411e8
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-11-28 20:56:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿double taban, yükseklik, alan;
+
+Console.WriteLine("Bu kod üçgenin alanını hesaplıyor");
+
+while (true)
+{
+
+    Console.WriteLine("Lütfen taban uzunluğunu giriniz:");
+    taban = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Lütfen yüksekliği giriniz:");
+    yükseklik = Convert.ToDouble(Console.ReadLine());
+
+    alan = taban * yükseklik / 2;
+
+    Console.WriteLine(alan);
+
+    Console.WriteLine("Programdan çıkmak için 'çıkış' yazın, devam etmek için herhangi bir tuşa basın");
+    string çıkış = Console.ReadLine();
+
+    if (çıkış == "çıkış")
+    {
+        break;
+    }
+
+}
+Console.WriteLine("İşlem sonlandırılıyor..");

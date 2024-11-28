6917a381b7ba40280dd3c8c16f095146a9a634d1
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-11-28 21:01:07 +0300

    Add files via upload

---
+++ Seçmeli (Optional) Parametre ile Belirli Yaştan Sonraki Yılları Sayma.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Sonuç(yaş belirtilmedi): " + YaşFarkı());
+        Console.WriteLine("Sonuç(yaş 25): " + YaşFarkı(25));
+        Console.WriteLine("Sonuç(yaş 18): " + YaşFarkı(18));
+    }
+    static int YaşFarkı(int yaş = 18)
+    {
+        return yaş - 18;
+    }
+}

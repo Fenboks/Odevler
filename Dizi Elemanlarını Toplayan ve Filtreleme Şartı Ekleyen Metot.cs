9757a23188d794ef1fc5b75792cd40ee4b7ec5aa
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-11-28 21:00:36 +0300

    Add files via upload

---
+++ Dizi Elemanlarını Toplayan ve Filtreleme Şartı Ekleyen Metot.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] sayılar = { 10, 15, 20, 25, 30 };
+        int filtre = 18;
+        int toplam = FiltreToplam(sayılar, filtre);
+        Console.WriteLine("Toplam: " + toplam);
+    }
+    static int FiltreToplam(int[] dizi, int filtre)
+    {
+        int toplam = 0;
+
+        foreach (int sayı in dizi)
+        {
+            if (sayı > filtre)
+            {
+                toplam += sayı;
+            }
+        }
+        return toplam;
+    }
+}

ee752c1b2b5d12387f13b59a3fd91a4ddff969bd
fenboks <148080250+Fenboks@users.noreply.github.com>
2024-11-28 21:01:44 +0300

    Add files via upload

---
+++ Geriye Koleksiyon Döndüren ve Veriyi Filtreleyen Metot.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string[] kelimeler = { "Hasan", "Berkay", "Muhammed", "Emir", "Ali", "Ahmet" };
+
+        List<string> uzunKelimeler = Filtrele(kelimeler);
+
+        Console.WriteLine("Uzunluğu 5'ten büyük kelimeler:");
+        foreach (string kelime in uzunKelimeler)
+        {
+            Console.WriteLine(kelime);
+        }
+    }
+    static List<string> Filtrele(string[] dizi)
+    {
+        List<string> sonuç = new List<string>();
+
+        foreach (string kelime in dizi)
+        {
+            if (kelime.Length > 5)
+            {
+                sonuç.Add(kelime);
+            }
+        }
+
+        return sonuç;
+    }
+}
